3715fd6d86afa2837148ce9ba8e12df3092adcda
Candace DeLorey <candacedelorey@gmail.com>
2014-03-11 14:20:15 -0300

    Make Timer currentValue label change with seekbar movement

--- app/src/main/java/cdelorey/speechtherapy/app/SetTimerDialogFragment.java
+++ app/src/main/java/cdelorey/speechtherapy/app/SetTimerDialogFragment.java
@@ -9,12 +9,16 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.content.Context;
 import android.view.ViewGroup;
+import android.widget.SeekBar;
+import android.widget.Toast;
+import android.widget.TextView;
 
 /**
  * Dialog fragment that prompts user to set the timer.
  */
 public class SetTimerDialogFragment extends DialogFragment {
     private Context context;
+    private TextView timerValue;
 
     public SetTimerDialogFragment(Context context) {
         this.context = context;
@@ -22,9 +26,30 @@ public class SetTimerDialogFragment extends DialogFragment {
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // get dialog layout
         LayoutInflater inflater = (LayoutInflater)
                 context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         View layout = inflater.inflate(R.layout.timer_dialog, null);
+
+        // setup seekbar
+        SeekBar seekBar = (SeekBar) layout.findViewById(R.id.timer_dialog_seekbar);
+        timerValue = (TextView) layout.findViewById(R.id.current_value);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                timerValue.setText("Timer length: " + progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+        });
+
+        // create dialog
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         builder.setMessage(R.string.dialog_set_timer)
                 .setView(layout)
