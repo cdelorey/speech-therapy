14dd864ec1638baaa90106011077ac101e2f55fb
Candace DeLorey <candacedelorey@gmail.com>
2014-03-12 15:18:24 -0300

    Make timer buttons turn green when pressed long enough and turn red when not pressed long enough

--- app/src/main/java/cdelorey/speechtherapy/app/TimerButton.java
+++ app/src/main/java/cdelorey/speechtherapy/app/TimerButton.java
@@ -7,6 +7,7 @@ import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.os.Handler;
 
 /**
  *
@@ -15,6 +16,20 @@ public class TimerButton extends Button {
     // Constants -----------------------------------------------------------------------------------
     private static final int TIMER_BUTTON_HEIGHT = 100;
     private static final int TIMER_BUTTON_WIDTH = 100;
+
+    // Data ----------------------------------------------------------------------------------------
+    private boolean timerIsRunning = false;
+    private int timerLength = 1000; // milliseconds
+    private Handler handler = new Handler();
+
+    private Runnable timer = new Runnable() {
+        @Override
+        public void run() {
+            // button has been held long enough
+            timerIsRunning = false;
+            TimerButton.this.setBackgroundColor(Color.BLUE);
+        }
+    };
 
 
     // Constructors --------------------------------------------------------------------------------
@@ -28,6 +43,14 @@ public class TimerButton extends Button {
         setupListener();
     }
 
+    // Setters -------------------------------------------------------------------------------------
+    public void setTimerLength(int length) {
+        timerLength = length;
+    }
+
+    public void clearTimer() {
+        handler.removeCallbacks(timer);
+    }
 
     // Private Methods -----------------------------------------------------------------------------
     private void setupListener() {
@@ -35,9 +58,16 @@ public class TimerButton extends Button {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
                 if(event.getAction() == MotionEvent.ACTION_DOWN) {
+                    // Start new timer
+                    handler.postDelayed(timer, timerLength);
+                    timerIsRunning = true;
                     TimerButton.this.setBackgroundColor(Color.GREEN);
                 } else if(event.getAction() == MotionEvent.ACTION_UP) {
-                    TimerButton.this.setBackgroundColor(Color.RED);
+                    // too slow
+                    if(timerIsRunning) {
+                        clearTimer();
+                        TimerButton.this.setBackgroundColor(Color.RED);
+                    }
                 }
                 return false;
             }
