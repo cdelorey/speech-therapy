8fe838e15e37fcdc62e9efafda974e46b15ca46b
cdelorey <cdelorey@fake.com>
2014-03-14 16:38:34 -0300

    Create TimerFragment abstract base class

--- app/src/main/java/cdelorey/speechtherapy/app/SingleButtonFragment.java
+++ app/src/main/java/cdelorey/speechtherapy/app/SingleButtonFragment.java
@@ -1,68 +1,41 @@
 package cdelorey.speechtherapy.app;
 
-import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.util.Log;
-import android.app.Activity;
 
 /**
  * Fragment with a single button.
  */
-public class SingleButtonFragment extends Fragment implements MainActivity.TimerCommunicator {
+public class SingleButtonFragment extends TimerFragment {
     private TimerButton singleButton;
 
     public SingleButtonFragment() {
 
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-
+    public View inflateView(LayoutInflater inflater, ViewGroup container) {
         View rootView = inflater.inflate(R.layout.fragment_single_button, container, false);
-
         Log.e(Constants.LOG, "View created");
         return rootView;
     }
 
-    @Override
-    public void onActivityCreated(Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
+    public void setupTimerButtons() {
         Log.e(Constants.LOG, "Fragment: OnActivityCreated");
         singleButton = (TimerButton) getActivity().findViewById(R.id.single_button);
     }
 
-    @Override
-    public void onAttach(Activity activity) {
-        super.onAttach(activity);
+    public void registerCommunicator() {
         MainActivity mainActivity = (MainActivity) getActivity();
         mainActivity.setSingleButtonFragmentCommunicator(this);
     }
 
-    @Override
-    public void onPause() {
-        super.onPause();
+    public void clearTimers() {
         singleButton.clearTimer();
     }
 
-    @Override
-    public void onResume() {
-        super.onResume();
-        Log.e(Constants.LOG, "Fragment: onResume");
-        getTimerFromActivity();
-    }
-
-    public void onChangeTimer(int milliseconds) {
-        singleButton.setTimerLength(milliseconds);
-    }
-
-    private void getTimerFromActivity() {
-        MainActivity activity = (MainActivity) getActivity();
-        int milliseconds = activity.getTimer();
-        Log.e(Constants.LOG, Integer.toString(milliseconds));
+    public void setTimers(int milliseconds) {
         singleButton.setTimerLength(milliseconds);
     }
 }
---
+++ app/src/main/java/cdelorey/speechtherapy/app/TimerFragment.java
@@ -0,0 +1,67 @@
+package cdelorey.speechtherapy.app;
+
+import android.app.Activity;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Base class for all fragments containing timerButtons
+ */
+public abstract class TimerFragment extends Fragment implements MainActivity.TimerCommunicator {
+
+    public TimerFragment() {
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflateView(inflater, container);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        setupTimerButtons();
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        registerCommunicator();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        clearTimers();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.e(Constants.LOG, "Fragment: onResume");
+        getTimeFromActivity();
+    }
+
+    public void onChangeTimer(int milliseconds) {
+        setTimers(milliseconds);
+    }
+
+    private void getTimeFromActivity() {
+        MainActivity activity = (MainActivity) getActivity();
+        int milliseconds = activity.getTimer();
+        Log.e(Constants.LOG, Integer.toString(milliseconds));
+        setTimers(milliseconds);
+    }
+
+    protected abstract View inflateView(LayoutInflater inflater, ViewGroup container);
+    protected abstract void setupTimerButtons();
+    protected abstract void registerCommunicator();
+    protected abstract void clearTimers();
+    protected abstract void setTimers(int milliseconds);
+}
